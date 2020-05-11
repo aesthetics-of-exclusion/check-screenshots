eb277cd4db0ab3d06e0b783c575e2f5c17b38024
Bert Spaan <bert.spaan@gmail.com>
2020-05-11 11:48:53 +0200

    initial version

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,155 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="poiId && annotation">
+      <img :src="annotation.data.screenshotUrl" />
+      <div class="buttons">
+        <button @click="submitCheck(true)">✅ Ja!</button>
+        <button @click="submitCheck(false)">❌ Nee!</button>
+      </div>
+    </div>
+    <div id="firebaseui-auth-container">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const firebase = require('firebase')
+const firebaseui = require('firebaseui')
+require('firebase/firestore')
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBBXAZEYCxqiPpCXpTH7XU_Vixg0ymZkfA',
+  authDomain: 'streetswipe-aoe.firebaseapp.com',
+  databaseURL: 'https://streetswipe-aoe.firebaseio.com',
+  projectId: 'streetswipe-aoe',
+  storageBucket: 'streetswipe-aoe.appspot.com',
+  messagingSenderId: '755260135363',
+  appId: '1:755260135363:web:4f3496bc29a8691c8b0f76'
+}
+
+const redirectUrl = process.env.VUE_APP_REDIRECT_URL
+
+const uiConfig = {
+  signInSuccessUrl: redirectUrl,
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID
+  ]
+}
+
+firebase.initializeApp(firebaseConfig)
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      poiId: undefined,
+      annotation: undefined
+    }
+  },
+  methods: {
+    getPoiRef: function (poiId) {
+      return this.db.collection('pois').doc(poiId)
+    },
+    submitCheck: async function (valid) {
+      await this.addAnnotation(this.poiId, 'check', {
+        valid
+      })
+    },
+    updateAnnotationCount: async function (poiId, type, increment) {
+      const poiRef = this.getPoiRef(poiId)
+      const poi = await poiRef.get()
+
+      let updatedPoiRef
+      if (increment !== undefined) {
+        const count = (poi.annotations && poi.annotations[type]) || 0
+        updatedPoiRef = await poiRef.update({
+          [`annotations.${type}`]: count + increment
+        })
+      } else {
+        updatedPoiRef = poiRef.update({
+          [`annotations.${type}`]: FieldValue.delete()
+        })
+      }
+
+      return updatedPoiRef
+    },
+    addAnnotation: async function (poiId, type, data) {
+      await this.updateAnnotationCount(poiId, type, 1)
+
+      const poiRef = this.getPoiRef(poiId)
+
+      const annotationRef = await poiRef.collection('annotations').doc().set({
+        poiId,
+        dateCreated: new Date(),
+        dateUpdated: new Date(),
+        type,
+        data
+      })
+
+      return annotationRef
+    },
+    loadScreenshot: async function () {
+      const poisRef = this.db.collection('pois')
+      const poisQuery = poisRef
+        .where('annotations.check', '==', 0)
+        .limit(1)
+
+      const poisSnapshot = await poisQuery.get()
+
+      if (poisSnapshot.docs.length) {
+        this.poiId = poisSnapshot.docs[0].id
+
+        const annotationsQuery = this.db.collection('pois')
+          .doc(this.poiId)
+          .collection('annotations')
+            .where('type', '==', 'screenshot')
+            .limit(1)
+
+        const annotationsSnapshot = await annotationsQuery.get()
+
+        this.annotation = annotationsSnapshot.docs[0].data()
+      } else {
+        console.log('nee!')
+      }
+    }
+  },
+  mounted: function () {
+    this.ui = new firebaseui.auth.AuthUI(firebase.auth())
+    this.ui.start('#firebaseui-auth-container', uiConfig)
+
+    this.db = firebase.firestore()
+
+    this.loadScreenshot()
   }
 }
 </script>
 
 <style>
+body {
+  padding: 10px;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 800px;
+  max-width: 100%;
+}
+
+img {
+  width: 100%;
+}
+
+.buttons {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.buttons button {
+  margin: 5px;
 }
 </style>
